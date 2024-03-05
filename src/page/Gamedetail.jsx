d0a813117139981579735e3a26d0b4600c90f0e1
Tyler Brooks <thetylerbrooks@gmail.com>
2024-03-05 15:57:17 -0700

    added useParams to Gamedetails instead of using props for typeName and GameId. We can then use those values to call game data from DB

--- src/page/Gamedetail.jsx
+++ src/page/Gamedetail.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+//we can access the gameId and typeName from the URL params (because they weren't passed as props from the handleNavigate function in Clubdetail)
+import { useParams } from "react-router-dom";
 
-export const Gamedetail = ({ gameId, typeName }) => {
-  // You can use gameId and typeName to fetch or display specific game details
+export const Gamedetail = () => {
+  const { gameId, typeName } = useParams();
+
+  // You can use gameId and typeName here to fetch or display specific game details
   return (
     <div>
       Gamedetail for {typeName} with ID {gameId}
